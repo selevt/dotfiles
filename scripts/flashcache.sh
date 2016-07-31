52826efae9363e075ae1a80fe0ebdadbca2509e3
selevt <selevt@selevt.com>
2016-07-31 17:49:18 +0200

    Remove flashcache script

--- scripts/flashcache.sh
+++
@@ -1,3 +0,0 @@
-lsof -u $(whoami) | grep Flash | awk '{sub(/[^0-9]+/, "", $4)
-                                       sub(/\/tmp\//, "", $9)
-                                      print "/proc/"$2"/fd/"$4, $9".flv"}' | xargs -rn 2 cp -n
